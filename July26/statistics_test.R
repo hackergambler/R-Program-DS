7c2da201fe3a0f4754eef7053e5bba2d2425fcaa
hackergambler <tibco.tibco.8@gmail.com>
2020-08-02 19:20:20 +0530

    logistic Regression

---
+++ July26/statistics_test.R
@@ -0,0 +1,20 @@
+Statistic test : 20  min : 50 marks
+1) out of 10 students , 7 students got admission in medical college . 
+a) what is the probability of students getting admission in medical college ? 
+  b ) what is the probability of students not getting admission in medical college ? 
+  2) what is the exponentiation of : 10^2 and  2^4 
+3) what is the logarithm ? please explain it with the help of an example . 
+4) what is normal distributrion ? 
+  5) what is the difference between left skewed and right skewed ? 
+  6) out of 25 marks in ds test , the scores received by the students is mentioned below : 
+  1) 5 
+2) 5
+3) 10 
+4) 15 
+5) 5 
+6) 20
+a) calculate the mean 
+b) calculate the mode 
+c) calculate the median 
+d) calculate the variance 
+e) calculate the standard deviation
